1514c8461a0fe6d15a7a6a8bd1a2783d059499e9
espicode <espigithub@yahoo.com>
2024-11-17 18:19:22 +0100

    Delete variable_arguments.scala

--- variable_arguments.scala
+++
@@ -1,6 +0,0 @@
-def printNumbers(numbers: Int*): Unit = {
-  numbers.foreach(println)
-}
-printNumbers(1,2,3)
-val arr = Array(1, 2, 3, 4)
-printNumbers(arr: _*) // Expands `arr` so each element is passed as a separate argument
