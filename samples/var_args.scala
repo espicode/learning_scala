7052bf04f00a4ffbd1ce94ee5ae51e7cd6fd4974
espicode <espigithub@yahoo.com>
2024-11-30 21:19:09 +0100

    Update var_args.scala

--- samples/var_args.scala
+++ samples/var_args.scala
@@ -1,6 +1,15 @@
+def min (a:Int, b:Int) : Int =
+ if a < b then a else b
 def printNumbers(numbers: Int*): Unit = {
   numbers.foreach(println)
 }
+
+// I call the function once with multiple parameters which can be read as a list
 printNumbers(1,2,3)
 val arr = Array(1, 2, 3, 4)
-printNumbers(arr: _*) // Expands `arr` so each element is passed as a separate argument
+
+// Expands `arr` so each element is passed as a separate argument
+printNumbers(arr: _*) 
+val a=7
+val b=0
+println(s"El minimimo de $a y $b es ${min(a,b)}")
