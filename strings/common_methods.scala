74e447f115a13b93b3f6f49afa7c35418fe7a6da
espicode <espigithub@yahoo.com>
2024-12-01 17:59:27 +0100

    Create common_methods.scala

---
+++ strings/common_methods.scala
@@ -0,0 +1,8 @@
+a.length // 12
+a.capitalize // "Hello, world"
+a.toUpperCase // "HELLO, WORLD"
+a.indexOf("h") // 0
+a.indexOf("e") // 1
+a.substring(0, 2) // "he"
+a.substring(0, 3) // "hel"
+a.substring(1, 3) // "el"
